ccb9985330a18b127f84670afe915e964b7fcad0
baconsanha <95085832+baconsanha@users.noreply.github.com>
2023-10-10 12:06:32 -0300

    adequando nome das variaves

--- distancia.c
+++ distancia.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
-//função que calcula a distancia
+//Funcao que calcula a distancia
 float calculaDistancia (int x1, int y1, int x2, int y2){
 	return sqrt( pow(x2-x1,2) + pow(y2-y1,2));
 }
 //main
 int main(){
-	//definição de variáveis
+	//Definicao de variaveis
 	int x1, y1, x2, y2;
 	float distancia;
-	//leitura das coordenadas dos pontos
+	//Leitura das coordenadas dos pontos
 	printf("Informe as coordenadas para imprimir o primeiro ponto: ");
 	scanf("%i %i", &x1, &y1);
 	printf("Informe as coordenadas para o segundo ponto: ");
 	scanf("%i %i", &x2, &y2);
 	
-	//calculo da distancia entre os pontos
+	//Calculo da distancia entre os pontos
 	distancia = calculaDistancia(x1, y2, x2, y2);
 	
-	//mostra o resultado
+	//Mostra o resultado
 	printf("Distancia: %f", distancia);
 }
